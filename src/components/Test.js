c36834319ae7089595d5fe40a57ca17530fcda9d
Reed MacDonald <reedpmacdonald@gmail.com>
2019-05-12 21:53:24 -0700

    added some stuff

--- src/components/Test.js
+++ src/components/Test.js
@@ -6,11 +6,9 @@ class Test extends Component{
     constructor(props){
         super(); 
         this.state = {
-
-            maze:[],
-            
-            
+            testing:false
         }
+        this.maze=[]
     }
 
     
@@ -23,20 +21,42 @@ class Test extends Component{
         this.setState({
             maze:
             testMaze.map((element)=>{
-            return <div><div className="cell" style={{'height':'5px','width':'5px','backgroundColor':element==1?'black':'white','margin':0}}></div></div>
+            return <div><Square testing={this.state.testing} className="cell" color={element=="1"?'black':'white'} style={{'height':'5px','width':'5px','backgroundColor':element==1?'black':'white','margin':0}}></Square></div>
         }
         )
     })
 
     }
 
+    outOfBounds = () =>{
+        alert('You are out of bounds!')
+    }
+
     componentDidMount(){
         this.getMaze()
+        this.startTimer()
+    }
+
+    theyFailed = () => {
+        alert('You have failed');
+        //Make a put route saying that they have failed
+    }
+
+    startTimer = () => {
+        setTimeout(this.theyFailed,120000)
+    }
+
+    attemptMaze = () => {
+        this.setState({
+            testing:true
+        })
+    }
+
+    submitMaze = (e) => {
+        e.preventDefault();
+        alert('You have submitted the maze')
     }
     
-
-
-        
   render(){
 
       return (
@@ -46,17 +66,47 @@ class Test extends Component{
         <div onMouseOver={(this.state.testing) ? this.outOfBounds:undefined} className="outOfBoundsTwo"></div>
         <div onMouseOver={(this.state.testing) ? this.outOfBounds:undefined} className="outOfBoundsThree"></div>
         <form onSubmit={this.handleSubmit}>
-        <button type="button" onClick={this.changeButtons} className="finishMaze" style={{'display':this.state.finishShowing}}>Finish Maze</button>
-        <button type="submit" className="finishMaze" style={{'display':this.state.submitShowing}}>Submit Maze</button>
+        <button onClick = {this.submitMaze} type="submit" className="finishMaze" style={{'display':this.state.submitShowing}}>Submit Maze</button>
         <div className="grid">
         {this.state.maze}
         </div>
         </form>
 
-        <button className="startEndButton">Attempt Maze</button>
+        <button type="submit" onClick={this.attemptMaze} className="startEndButton">Attempt Maze</button>
         </div>
         
   )}
+}
+
+class Square extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            scar: this.props.scar,
+          color: this.props.color,
+          colorToChange:null,
+          changeColor: false,
+          clicked:this.props.clicked
+        };
+      }
+    
+    hitWall = () => {
+        alert('you hit a wall')
+    }
+
+    switchColor = ()=>{
+        this.state.color=='black' && alert('You hit a wall')
+        this.setState({
+            color:'red'
+        })
+    }
+
+    render(){
+        return(
+            <div className="boxes" onMouseOver = {!this.props.testing?this.switchColor:undefined} style={{'height':'5px','width':'5px','backgroundColor':this.state.color,'margin':'0'}}>
+            </div>
+        )
+    }
 }
 
 
--- src/components/bestOf.js
+++ src/components/bestOf.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+let allMazes
 
 class TopMazes extends Component{
+    constructor(props){
+        super(); 
+        this.state = {
+            bestMazes:[]
+            
+        }
+    }
+    
+    
+    getBestOf = async (e) => {
+        const bestofResponse = await fetch('/maze/all')
+        const parsedResponse = await bestofResponse.json();
+        allMazes = parsedResponse.data
+        console.log(allMazes)
+        this.setState({
+            bestMazes:allMazes.map((element)=>
+            <li>{element.name}</li>)
+        })
+        return allMazes
+    }
+
+    componentDidMount(){
+        this.getBestOf()
+    }
     render(){
+
         return(
             <div>
             <h1>Top Mazes</h1>
             <ol>
-                <li>Best maze 1</li>
-                <li>Best Maze 2</li>
-                <li>Best Maze 3</li>
-                <li>Best maze 4</li>
+                <li>{this.state.bestMazes}</li>
             </ol>
             </div>
         )
